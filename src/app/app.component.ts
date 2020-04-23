49d0083213eef502cb724910f57bbcb2b43e22c8
mohankumar1u <mohankumar.uppu@gmail.com>
2020-04-23 22:08:35 +0530

    3 files modified, 1 added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
-
+import { Component,OnInit } from '@angular/core';
+import {AppService} from './app.service'
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  styleUrls: [ './app.component.css' ],
+  providers:[AppService]
 })
 export class AppComponent  {
   name = 'Angular';
+  cards:any;
+  constructor(private AppService:AppService ){}
+  ngOnInit(){
+     this.getCardDetails1()
+  }
+  getCardDetails1 =()=>{
+    this.AppService.getCardDetails().subscribe(element=>{
+      this.cards=element;
+      console.log(this.cards[0]);
+    });
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,11 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HelloComponentModule } from './hello.component';
-
+import { CardsComponent } from '../cards/cards.component';
+import {HttpClientModule} from '@angular/common/http'
 @NgModule({
-  imports:      [ BrowserModule, FormsModule, HelloComponentModule ],
-  declarations: [ AppComponent ],
+  imports:      [ BrowserModule, FormsModule, HelloComponentModule, HttpClientModule ],
+  declarations: [ AppComponent, CardsComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
