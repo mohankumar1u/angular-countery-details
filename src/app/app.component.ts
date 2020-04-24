d58471383bdaabfe4ab0276c78bb3fdbb8e7716a
mohankumar1u <mohankumar.uppu@gmail.com>
2020-04-24 19:01:25 +0530

    1 file modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,29 +1,40 @@
-import { Component,OnInit,Pipe, PipeTransform } from '@angular/core';
-import {AppService} from './app.service'
+import { Component, OnInit, Pipe, PipeTransform } from "@angular/core";
+import { AppService } from "./app.service";
 @Component({
-  selector: 'my-app',
-  templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ],
-  providers:[AppService]
+  selector: "my-app",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"],
+  providers: [AppService]
 })
 // @Pipe({
 //     name: 'filter'
 // })
 //@Injectable()
-
-export class AppComponent  {
-  name = 'Angular';
-  cards:any;
-   title = 'Angular Search Using ng2-search-filter';
+export class AppComponent {
+  name = "Angular";
+  cards: any;
+  title = "Angular Search Using ng2-search-filter";
+  region=[];
+  i=0;
   searchText;
-  constructor(private AppService:AppService ){}
-  ngOnInit(){
-     this.getCardDetails1()
+  constructor(private AppService: AppService) {}
+  ngOnInit() {
+    this.getCardDetails1();
   }
-  getCardDetails1 =()=>{
-    this.AppService.getCardDetails().subscribe(element=>{
-      this.cards=element;
-      console.log(this.cards[0]);
+  getCardDetails1 = () => {
+    this.AppService.getCardDetails().subscribe(element => {
+      this.cards = element;
+      let obj =[];
+      for (let [key, value] of Object.entries(this.cards))
+       {
+          obj.push(value.region);
+        // Object.assign(this.region , {key: value.region});
+      }
+      this.region = obj.filter(function(item, pos, self) {
+    return self.indexOf(item) == pos;
+})
+         console.log(this.region)
+
     });
-  }
+  };
 }
